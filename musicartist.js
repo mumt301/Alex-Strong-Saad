f303ce960da2a4f6230eb898945f5c7101de13a8
Alex Strong <saadalex9@gmail.com>
2022-11-19 17:13:57 -0500

    commit

--- musicartist.js
+++ musicartist.js
@@ -13,20 +13,14 @@ function generate_results() {
         let queryURL = `http://musicbrainz.org/ws/2/artist/?query=${artist}`;
         console.log(queryURL);
         httpGet(queryURL, getMBID);
-
+    } }
         // 1. Making the query (https://www.w3schools.com/js/js_ajax_http.asp - last two tables)
         let xmlHttp = new XMLHttpRequest(); // Create an XMLHttpRequest object
         xmlHttp.open("GET", queryURL); // Set the request method and URL
         xmlHttp.send(); // Initiates (sends) the request
         // Defines a function to be called when the readyState property changes
         xmlHttp.onreadystatechange = function() {
-            if(this.readyState == 4 && this.status == 200){
-                // readyState: holds the status of the XMLHttpRequest (0: request not initialized, 1: server connection established, 2: request received, 3: processing request, 4: request finished and response is ready)
-                // status: returns the status-number of a request (200: "OK", 403: "Forbidden", 404: "Not Found")
-                let retrievedData = this.responseXML; // Returns the response as document (XML data)
-                console.log(retrievedData);
-                
-            }
+            if(this.readyState == 4 && this.status == 200)
 
             function getMBID(xhttp) {
                 let retrievedData = xhttp.responseXML;
@@ -56,6 +50,8 @@ function generate_results() {
                     console.log(AlbumNames);
                     let AlbumDates = data.getElementsByTagName("first-release-date")[0].innerHTML;
                     console.log(AlbumDates);
+                    
+                    
                     table += "<tr><td>" + AlbumNames + "</td>" + "<td>" + AlbumDates + "</td><tr>"
                     }
                     table += "</table>" 
@@ -63,7 +59,6 @@ function generate_results() {
                     }
             }
         };
-    }
-}
+    
 
 window.onload = generate_results();
