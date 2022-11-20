d523cbf2877c2f9020a2f5e90118eb89dfa834ae
Alex Strong <saadalex9@gmail.com>
2022-11-19 22:13:57 -0500

    assignment6

--- musicapi.js
+++ musicapi.js
@@ -3,7 +3,9 @@ function generate_results() {
     if (params.has('artist')) {
         let artistName = params.get('artist');
         console.log(artistName);
-        let queryURL = 'https://musicbrainz.org/ws/2/artist?query=${artist}';
+        let mbBaseURL = "https://musicbrainz.org/ws/2/";
+        let mbResource = "artist?query=";
+        let queryURL = mbBaseURL + mbResource + artistName;
         console.log(queryURL);
         httpGet(queryURL, getMBID);
     }
@@ -26,32 +28,47 @@ function getMBID(xhttp) {
     console.log(retrievedData);
     let artistData = retrievedData.getElementsByTagName('artist')[0];
     console.log(artistData);
-    let artistData = artistData.getElementsByTagName('name')[0].innerHTML;
-    console.log(artistData);
+    let artistName = artistData.getElementsByTagName('name')[0].innerHTML;
+    console.log(artistName);
     let artistMBID = artistData.id;
     console.log(artistMBID);
-    let queryURL = 'https://musicbrainz.org/ws/2/release-group?artist=${artistMBID}'; 
+    let mBaseURL = "https://musicbrainz.org/ws/2/release-group?artist="; 
+    let queryURL = mBaseURL + artistMBID;
     console.log(queryURL);
     httpGet(queryURL,getAlbums);
     getAlbums(artistMBID)
 }
 
+function getAlbum(xhttp) {
+    var AlbumNames = [];
+    var AlbumDates = [];
+    let data = xhttp.responseXML;
+    console.log(data);
 
-function getAlbums(xhttp) {
-   let retrievedData = xhttp.responseXML;
-   console.log(retrievedData);
-   let albums=retrievedData.getElementsByTagName('release-group');
-   let placeholder = document.getElementById('placeholder');
-   let table = "<table><tr><th>Title</th><th>Date</th></tr>";
-   for (let row = 0; row < albums.length; row++){
-   let data = albums[row];
-   let AlbumNames = data.getElementsByTagName("title")[0].innerHTML;
-   console.log(AlbumNames);
-   let AlbumDates = data.getElementsByTagName("first-release-date")[0].innerHTML;
-   console.log(AlbumDates);
-   table += "<tr><td>" + AlbumNames + "</td>" + "<td>" + AlbumDates + "</td><tr>"
-   }
-   table += "</table>" 
-   placeholder.innerHTML = table;
-   }
+    let release_list = data.getElementsByTagName("release-group-list")[0];
+    console.log(release_list);
+    let releaseGroups = release_list.getElementsByTagName("release-group");
+    let releaseCount = releaseGroups.length;
+    document.getElementById("albums").innerHTML = releaseCount + " results";
+
+    console.log(releaseCount)
+        for (let index = 0; index < releaseCount; index++) {
+    let album_Dt = release_list.getElementsByTagName("release-group")[index];
+    let albumName = album_Dt.getElementsByTagName('title')[0].innerHTML;
+    console.log(albumName);
+    AlbumNames[index] = albumName;
+    let albumDate = album_Dt.getElementsByTagName('first-release-date')[0].innerHTML
+    console.log(albumDate);
+    AlbumDates[index] = albumDate;
+    }
+    console.log(AlbumNames);
+    console.log(AlbumDates);
+
+    text = "<tr><th>Album</th><th>Release Date</th></tr>";
+        for (i = 0; i < AlbumNames.length; i++) {
+        text += "<tr><td> " + AlbumNames[i] + "</td>";
+        text += "<td> " + AlbumDates[i] + "</td></tr>";
+    }
+    let placeholder = document.getElementById('placeholder');
+    placeholder.innerHTML = text;
 window.onload = generate_results;
