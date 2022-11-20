5ed7d08210d938d9640de2c48e9f824e857bd394
Alex Strong <saadalex9@gmail.com>
2022-11-19 19:54:33 -0500

    musicapi.js

--- musicapi.js
+++ musicapi.js
@@ -3,7 +3,9 @@ function queryArtist() {
     if (params.has('artist')) {
         let artistName = params.get('artist');
         console.log(artistName);
-        let queryURL = "https://musicbrainz.org/ws/2/artist?query=${artistName}";
+        let mbBaseURL = "https://musicbrainz.org/ws/2/artist?query=";
+        let mbResource = "artist?query=";
+        let queryURL = mbBaseURL + mbResource + artistName;
         console.log(queryURL);
         httpGet(queryURL, getMBID);
     }
