6cd232e355bf84ee3d3ea59ccda16fcac46fc506
Alex Strong <saadalex9@gmail.com>
2022-11-26 19:30:49 -0500

    sike

--- 11.js
+++ 11.js
@@ -26,7 +26,7 @@ function thereminControl(e, oscillator, theremin) {
     let note = document.getElementById("note");
 
     frequency.innerHTML= thereminFreq + " Hz"
-    note.innerHTML= noteFrameFrequency(frequency, true)
+    note.innerHTML= noteFromFrequency(frequency, true)
 }
 
 // Turn theremin off
